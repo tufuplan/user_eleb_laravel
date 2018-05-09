87bf8ad6b3eaa294205263c3ea96af98c0509d03
phpzhou <hjhhj>
2018-05-09 16:42:06 +0800

    修改根目录指向login

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index.html');
 });
 Route::get('/shops','ApiController@shops');
 Route::get('/shop','ApiController@shop');
